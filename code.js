803c4396a051fc8cfb9085272b11fa6c5a459dd4
Fadi Haddad <fadi.haddad.235@gmail.com>
2023-02-25 21:52:07 +0200

    added eventListener on Signup button

--- code.js
+++ code.js
@@ -5,6 +5,13 @@ const lname = document.getElementById("lname");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const number = document.getElementById("number");
+const password_icon =document.getElementById("password-status");
+const email_icon =document.getElementById("email-status");
 
+signup_button=document.getElementById("signup-button");
+signup_button.addEventListener("click",validate);
+
+// i.classList.add("fa-check");
+// i.classList.add("fa-xmark");
 
 }
