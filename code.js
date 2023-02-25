7c4e90a3bdbbc80a47f2ba7214a0b36794d7efab
Fadi Haddad <fadi.haddad.235@gmail.com>
2023-02-25 21:21:04 +0200

    added <i> elements

---
+++ code.js
@@ -0,0 +1,10 @@
+window.onload=function(){
+const form = document.getElementById("form");
+const fname = document.getElementById("fname");
+const lname = document.getElementById("lname");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const number = document.getElementById("number");
+
+
+}
